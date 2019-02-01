632f546463904e6c4a6ed2b0941e995e80dbdc07
Kirill Leontev <kyrary99@gmail.com>
2019-02-01 18:35:32 +0200

    sec commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('mytopnav');
+
+	if(x.className === "topnav"){
+		x.className += " responsive"
+	}
+	else{
+		x.className = "topnav"
+	}
+	// body...
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+menu.onclick = function myFunction() {
+	var x = document.getElementById('mytopnav');
+
+	if(x.className === "topnav") {
+		x.className += " responsive";
+	}
+	else{
+		x.className = "topnav";
+	}
+}
